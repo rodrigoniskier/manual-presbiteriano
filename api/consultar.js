077e3599ecf6b7bd863204b66fa138d8ddb330f6
dev.Rodrigo Niskier <niskier.rodrigo@gmail.com>
2026-01-15 10:36:35 -0300

    Change model name from 'gemini-3-flash' to 'gemini-2.5-flash'

--- api/consultar.js
+++ api/consultar.js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
         // Opção 2 (Inteligente - Lenta): 'gemini-1.5-pro'
         // Opção 3 (A do seu PDF): 'gemini-3-pro-preview' (Cuidado: pode não estar ativa ainda e dar erro 404)
         
-        const modelName = 'gemini-3-flash'; // <--- Mude aqui para testar outros
+        const modelName = 'gemini-2.5-flash'; // <--- Mude aqui para testar outros
 
         const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/${modelName}:generateContent?key=${apiKey}`, {
             method: 'POST',
